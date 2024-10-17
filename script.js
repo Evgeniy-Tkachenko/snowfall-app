b2ac408b138c5735e5baf90c2361dc742328a82b
Евгений <tkevru@gmail.com>
2024-10-17 17:41:00 +0300

    Снеговик

--- script.js
+++ script.js
@@ -5,7 +5,44 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 let snowflakes = [];
+let snowman = {
+    x: 50,
+    y: canvas.height -20,
+    size: 20,
+    speed: 2,
+}
 
+function drawSnowman() {
+
+    //тело
+    context.beginPath();
+    context.arc(snowman.x, snowman.y, snowman.size, 0, Math.PI * 2);
+    context.fillStyle = 'white';
+    context.fill();
+
+    // голова
+    context.beginPath();
+    context.arc(snowman.x, snowman.y - snowman.size * 1.5, snowman.size / 1.5, 0, Math.PI * 2);
+    context.fill();
+
+    // глаза
+    context.fillStyle = 'black';
+    context.beginPath();
+    context.arc(snowman.x - 5, snowman.y - snowman.size * 1.5 - 5, 2, 0, Math.PI * 2);
+    context.fill();
+
+    context.beginPath();
+    context.arc(snowman.x + 5, snowman.y - snowman.size * 1.5 - 5, 2, 0, Math.PI * 2);
+    context.fill();
+}
+
+function moveSnowman() {
+    snowman.x += snowman.speed;
+
+    if (snowman.x > canvas.width - snowman.size || snowman.x < snowman.size) {
+        snowman.speed *= -1;
+    }
+}
 
 function createSnowflakes() {
     const numFlakes = 100;
@@ -38,6 +75,8 @@ function animateSnowflakes() {
         context.fill();
     });
 
+    drawSnowman();
+    moveSnowman()
     requestAnimationFrame(animateSnowflakes);
 }
 
